aec0d238ac9035cdcbaaec0f400f9b0932b192bf
Adel Jojo <bigfanjs@gmail.com>
2017-07-27 12:54:29 +0100

    feat: creates a Widget placeholder component

---
+++ src/components/widget/app/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Header from '../header';
+import Body from '../body';
+import Footer from '../footer';
+
+export default function () {
+  return (
+    <div>
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  );
+}
