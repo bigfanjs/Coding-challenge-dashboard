5f7672d33508773578694dfdd0d89da37f57a059
Adel Jojo <bigfanjs@gmail.com>
2017-07-27 12:27:26 +0100

    feat: creates a placeholder action-bar component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Header from './components/header';
+import ActionBar from './action-bar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <ActionBar />
       </div>
     );
   }
---
+++ src/components/action-bar/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function () {
+  return <div>Action Bar Placeholder Component!</div>;
+}
