21df3ec4f31b3fc598cf27748eb2b4ba45e13c65
Adel Jojo <bigfanjs@gmail.com>
2017-07-28 13:22:27 +0100

    feat: adds the pie chart colors

--- src/components/heighest-mobile-users/app/app.js
+++ src/components/heighest-mobile-users/app/app.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import Widget from '../../widget';
 import UsersList from '../users-list';
 import PieChart from '../pie-chart';
 
-export default function ({ users }) {
-  return (
-    <Widget title="Heighest Mobile Users">
-      <UsersList users={users} />
-      <PieChart />
-    </Widget>
-  );
+class HeighestMobileUsers extends Component {
+  constructor(props) {
+    super(props);
+
+    this.colors = [
+      '#ffc107',
+      '#E91E63',
+      '#35d5f6',
+      '#4d5cef',
+      '#009688'
+    ];
+  }
+
+  render() {
+    return (
+      <Widget title="Heighest Mobile Users">
+        <UsersList
+          colors={this.colors}
+          users={this.props.users}/>
+        <PieChart colors={this.colors} />
+      </Widget>
+    );
+  }
 }
+
+export default HeighestMobileUsers;
